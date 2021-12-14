19a4b5de8076ea1f839d039f94e71594f6ff4080
DiegoGonzy013 <boria37diego@gmail.com>
2021-12-14 10:37:17 +0100

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from'react';
+import React from "react";
 
 import ComponenteEj from './components/ComponenteEj';
 
--- src/components/ComponenteEj.jsx
+++ src/components/ComponenteEj.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 export default function ComponenteEj (props){
   return <div>Mi primer componente</div>;
