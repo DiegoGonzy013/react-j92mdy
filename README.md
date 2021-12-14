213e217b97d31e174658f59c904b87eeb2e85cc5
DiegoGonzy013 <boria37diego@gmail.com>
2021-12-14 10:33:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-j92mdy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-j92mdy)
---
+++ src/components/ComponenteEj.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ComponenteEj (props){
+  return <div>Mi primer componente</div>;
+}
