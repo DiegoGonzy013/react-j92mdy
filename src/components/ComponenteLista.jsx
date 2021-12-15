d6cfe67de0d898d105de2ffd6e0c983fac7bbe75
DiegoGonzy013 <boria37diego@gmail.com>
2021-12-15 13:04:36 +0100

    3 files modified

--- src/components/ComponenteLista.jsx
+++ src/components/ComponenteLista.jsx
@@ -1,4 +1,4 @@
 import React from "react";
 export default function ComponenteLista(props){
- return <div><li>{props.texto}</li></div>
+ return <li>{props.texto}</li>
 }
--- src/components/Lista.jsx
+++ src/components/Lista.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import ComponenteLista from './ComponenteLista';
 
 export default function Lista(props) {
+  const listaComponentes = [];
+  for (let i = 0; i < props.elementos.length; i++) {
+    listaComponentes.push(<ComponenteLista texto="Se trata de otro elemento posiblemente el siguiente " />);
+  }
   return (
   <div>
   {props.titulo} - {props.icono}
     <ul>
       <ComponenteLista texto="Elemento 1"/>
       <ComponenteLista texto="Elemento 2"/>
-      <li>Elemento 3</li>
-
+      <ComponenteLista texto="Elemento 3"/>
+      {listaComponentes}
     </ul>
   </div>);
 }
